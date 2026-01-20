bdab206b74503ea215e4f225bfc1905666a47fdc
Mehakpreet Kaur <mehak18preet@gmail.com>
2026-01-20 21:47:02 +0530

    permitting role api for everyone

--- backend/HotelBookingApplication/src/main/java/com/HotelBookingApplication/security/WebSecurityConfig.java
+++ backend/HotelBookingApplication/src/main/java/com/HotelBookingApplication/security/WebSecurityConfig.java
@@ -63,7 +63,7 @@ public class WebSecurityConfig {
                         exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/rooms/**","/bookings/**")
+                        .requestMatchers("/auth/**", "/rooms/**","/bookings/**","/roles/**")
                         .permitAll().requestMatchers("/roles/**").hasRole("ADMIN")
                         .anyRequest().authenticated());
         http.authenticationProvider(authenticationProvider());
